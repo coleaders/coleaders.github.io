49383f80029ea880e73308bf55372c5913415c98
YUKAFUMI <unit@yukafumi.net>
2021-04-20 10:59:04 +0900

    Renewal file

---
+++ js/common.js
@@ -0,0 +1,78 @@
+
+// 画像切り替え
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+
+
+// ページ内のスクロール
+$(function() {
+  // スクロールのオフセット値
+  var offsetY = -10;
+  // スクロールにかかる時間
+  var time = 500;
+  // ページ内リンクのみを取得
+  $('a[href^=#]').click(function() {
+    var target = $(this.hash);
+    if (!target.length) return ;
+    var targetY = target.offset().top+offsetY;
+    $('html,body').animate({scrollTop: targetY}, time, 'swing');
+    window.history.pushState(null, null, this.hash);
+    return false;
+  });
+});
+
+
+
+// ページトップへのスクロール
+$(function() {
+    var topBtn = $('.page-top');    
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
+
+
+// ナビゲーション
+jQuery(document).ready(function(){
+
+  jQuery('.hamburger').on('click', function(){
+   jQuery('.navigation').animate({width: 'toggle'}, 400);
+    jQuery(this).toggleClass('active');
+  });
+
+});
+
+
